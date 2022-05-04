b830cacb32627f1d7ab171ffa21162d7a5231549
Mugisha-isaac <mugishaisaac2001@gmail>
2022-05-04 20:30:14 +0200

    feat(exceptions/) adding exceptions

--- src/main/java/dev/isaac/steramschedule/model/LiveStream.java
+++ src/main/java/dev/isaac/steramschedule/model/LiveStream.java
@@ -1,6 +1,111 @@
 package dev.isaac.steramschedule.model;
 
-import java.time.LocalDateTime;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-public record LiveStream(String id, String title, String description, String url, LocalDateTime startDate, LocalDateTime endDate) {
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+@Entity
+@Table(name = "streams")
+@EntityListeners(AuditingEntityListener.class)
+public class LiveStream{
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+    @Column(name = "title", nullable = false)
+    private String title;
+    @Column(name = "description", nullable = false)
+    private String description;
+    @Column(name = "url", nullable = false)
+    private String url;
+    @Column(name = "start_date", nullable = false)
+    private LocalDateTime startDate;
+    @Column(name = "end_date", nullable = false)
+    private LocalDateTime endDate;
+
+    public LiveStream() {
+    }
+
+    public LiveStream(Integer id, String title, String description, String url, LocalDateTime startDate, LocalDateTime endDate) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.url = url;
+        this.startDate = startDate;
+        this.endDate = endDate;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public LocalDateTime getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(LocalDateTime startDate) {
+        this.startDate = startDate;
+    }
+
+    public LocalDateTime getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(LocalDateTime endDate) {
+        this.endDate = endDate;
+    }
+
+    @Override
+    public String toString() {
+        return "LiveStream{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", description='" + description + '\'' +
+                ", url='" + url + '\'' +
+                ", startDate=" + startDate +
+                ", endDate=" + endDate +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        LiveStream that = (LiveStream) o;
+        return id.equals(that.id) && title.equals(that.title) && description.equals(that.description) && url.equals(that.url) && startDate.equals(that.startDate) && endDate.equals(that.endDate);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, title, description, url, startDate, endDate);
+    }
 }
