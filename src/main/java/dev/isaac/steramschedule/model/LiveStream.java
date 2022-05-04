dc32ba1bbd4a458bfac8172f635173c66281ef18
Mugisha-isaac <mugishaisaac2001@gmail>
2022-05-04 12:19:26 +0200

    feat(model/) implementing model class

--- src/main/java/dev/isaac/steramschedule/model/LiveStream.java
+++ src/main/java/dev/isaac/steramschedule/model/LiveStream.java
@@ -3,5 +3,4 @@ package dev.isaac.steramschedule.model;
 import java.time.LocalDateTime;
 
 public record LiveStream(String id, String title, String description, String url, LocalDateTime startDate, LocalDateTime endDate) {
-
 }
